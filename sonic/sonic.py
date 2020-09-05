fd6bb7c7fc988c1b01347f3ec7cf38b6d6c64425
Chimychanga <neville_nsw@hotmail.com>
2020-09-05 23:16:43 +1000

    Added working preview for both preprocessed and original image

--- sonic/sonic.py
+++ sonic/sonic.py
@@ -1,4 +1,22 @@
 from retro_contest.local import make
+import cv2
+import time
+import numpy as np
+
+#preprocess image
+def preprocess(img):
+    to_return = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+    to_return = cv2.resize(to_return, (96,96), cv2.INTER_AREA)
+    return to_return
+
+#creates framestack or if given, adds framestack
+def framestack(framestack,frame):
+    if framestack:
+        framestack.add(preprocess(frame))
+    #else:
+        #Create empty framestack here with preprocess
+    pass
+
 
 
 def main():
@@ -6,11 +24,20 @@ def main():
     obs = env.reset()
     while True:
         obs, rew, done, info = env.step(env.action_space.sample())
-        env.render()
+        #env.render()
+        obs = cv2.cvtColor(obs, cv2.COLOR_RGB2BGR)
+        grey_3_channel = cv2.cvtColor(preprocess(obs), cv2.COLOR_GRAY2BGR)
+        grey_3_channel = cv2.resize(grey_3_channel, (320,224))
+        numpy_horizontal_concat = np.concatenate((obs, grey_3_channel), axis=1)
+
+        cv2.imshow('Sonic',numpy_horizontal_concat)
+        cv2.waitKey(1)
+        time.sleep(0.1)
+
         if done:
             obs = env.reset()
 
 
+
 if __name__ == '__main__':
     main()
-
